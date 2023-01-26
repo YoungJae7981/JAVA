bd25eae2ccbd6d0c59f93a0d88c5d38486597145
sample <youngjae7981@gmail.com>
2023-01-26 17:16:14 +0900

    🎨 ssss

--- js/Variable.js
+++ js/Variable.js
@@ -1,12 +1,14 @@
 // var, let&const차이점
 // 변수에 어떤값을 넣기 위헤서 확보한 공간
-// 연산자로 변한값
+// 연산자로 변tndp rkqtdmf gkfekd
 
 
+// var, let은 변수 선언후 값 재할당 가능
 let a =123;
 a=456;
 console.log(a);
-
+// const 는 변수 선언후 값 재할당 불가능
+// 유저정보,절대 변해서는 안되는 값
 const b =123;
 b=456;
 console.log(b);
